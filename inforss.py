41c76cc46f4db795e1a2e0163d443596ef6bea76
1183519e <lukas+bitbucket@luftamt-hersbruck.de>
2018-03-06 09:23:54 +0100

    thingies

--- inforss.py
+++ inforss.py
@@ -7,24 +7,42 @@ import pprint
 pp = pprint.PrettyPrinter(indent=4)
 
 with open('key') as file:
-	KEY = file.read()
+	KEY = file.read().rstrip()
 
 RSS_URL = "http://www.tagesschau.de/xml/rss2"
+INFOBEAMER_URL = "https://info-beamer.com/api/v1/setup/4972"
 
-buffer = BytesIO()
+rssbuffer = BytesIO()
+postbuffer = BytesIO()
 
-curl = pycurl.Curl()
+### Get news articles from rss feed
+curl =  pycurl.Curl()
 curl.setopt(curl.URL, RSS_URL)
-curl.setopt(curl.WRITEDATA, buffer)
+curl.setopt(curl.WRITEDATA, rssbuffer)
 curl.perform()
+del curl
 
-#print(buffer.getvalue().decode('utf8'))
-
-tree = ET.fromstring(buffer.getvalue().decode('utf8'))
+### Parse into info-beamer format (json)
+tree = ET.fromstring(rssbuffer.getvalue().decode('utf8'))
 news = ()
 for item in tree.findall("*/item")[0:4]:
 	title = item.find("title").text
 	description = item.find("description").text	
 	news = news + ({"title":title, "description":description},)
 
-pp.pprint(news)
+data = 'config={"scroller":{"texts":['
+for item in news:
+	data = data + '{"text":"' + item["description"] + '"},'
+data = data + ']}}&mode=update'
+
+
+### curl curly data into fluffy clouds
+curl = pycurl.Curl()
+curl.setopt(curl.URL, INFOBEAMER_URL)
+curl.setopt(curl.USERPWD, ":" + KEY)
+curl.setopt(curl.POST, 1)
+curl.setopt(curl.POSTFIELDS, data.encode('utf-8'))
+curl.setopt(curl.WRITEDATA, postbuffer)
+curl.perform()
+del curl
+print(postbuffer.getvalue())
